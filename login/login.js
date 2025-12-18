da4583e773343017e211d6b9943f958151abe4b8
pashinina <yanapashinina379@gmail.com>
2025-12-18 12:19:15 +0200

    first commit

---
+++ login/login.js
@@ -0,0 +1,31 @@
+async function login() {
+  try {
+    const res = await fetch('https://matrix.org/_matrix/client/r0/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        type: 'm.login.password',
+        user: this.username,
+        password: this.password
+      })
+    });
+
+    const data = await res.json();
+
+    if (data.access_token) {
+      this.accessToken = data.access_token;
+      this.userId = data.user_id;
+      await this.fetchRoomsWithNames();
+      this.fetchMessages && this.fetchMessages();
+      setInterval(() => {
+        this.fetchRoomsWithNames();
+        this.fetchMessages && this.fetchMessages();
+      }, 5000);
+    } else {
+      this.error = '❌ Невірний логін або пароль';
+    }
+  } catch (e) {
+    this.error = '⚠️ Помилка під час входу: ' + e.message;
+    console.error(e);
+  }
+}
